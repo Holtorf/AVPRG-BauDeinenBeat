665681331044616a59ecf0ea76486804da9504c8
Vincent Holtorf <vholtorf@gmx.de>
2018-11-15 12:25:12 +0100

    Add functions
    Slider ändern ihren wert und die ersten Button sind implementiert

---
+++ JavaScript/avprgProject.js
@@ -0,0 +1,63 @@
+var context = new AudioContext(),
+    sliders = document.getElementsByClassName("slider"),
+    
+	lpButton = document.getElementById("lpButton"),
+    hpButton = document.getElementById("hpButton"),
+    bpButton = document.getElementById("bpButton"),
+    apButton = document.getElementById("apButton"),
+    lsButton = document.getElementById("lsButton"),
+    hsButton = document.getElementById("hsButton"),
+    pButton = document.getElementById("pButton"),
+    nButton = document.getElementById("nButton"),
+
+    startBtn = document.getElementById("startBtn"),
+	saveBtn = document.getElementById("saveBtn"),
+    stopBtn = document.getElementById("stopBtn"),
+
+    isPlaying = false,
+   
+	sound = new Audio("../sounds/sound.wav"),
+	source = context.createMediaElementSource(sound),
+	filter = context.createBiquadFilter();
+
+   for (var i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener("mousemove", changeParameter);
+}
+
+function changeParameter() {
+    switch(this.id) {
+        case "frequencySlider":
+            filter.frequency.value = this.value;
+            document.getElementById("frequencyOutput").innerHTML = this.value + " Hz";
+            break;
+        case "detuneSlider":
+            filter.detune.value = this.value;
+            document.getElementById("detuneOutput").innerHTML = this.value + " Cents";
+            break;
+        case "qSlider":
+            filter.Q.value = this.value;
+            document.getElementById("qOutput").innerHTML = this.value;
+            break;
+        case "gainSlider":
+            filter.gain.value = this.value;
+            document.getElementById("gainOutput").innerHTML = this.value + " dB";
+            break;
+    }
+}
+
+startBtn.addEventListener("click",function(){
+	sound.play();
+});
+
+stopBtn.addEventListener("click",function(){
+	sound.pause();
+});
+
+saveBtn.addEventListener("click",function(){
+
+});
+
+lpButton.addEventListener("click",function(){
+ filter.type = filter.LOWPASS;
+ filter.frequency.value = 5000;
+});
