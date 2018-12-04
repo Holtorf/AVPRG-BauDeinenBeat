da37aabd47e0d17a543a8ab34102a0cb1f8ea1f4
Vincent Holtorf <vholtorf@gmx.de>
2018-12-04 16:27:18 +0100

    Vince C++ Ordner && Midi
    alte ordner gelöscht und Midi verändert

--- JavaScript/midi.js
+++ JavaScript/midi.js
@@ -1,10 +1,20 @@
+	var context = new AudioContext(),
 	rSound = new Audio("../Sounds/banjo.wav"),
 	gSound = new Audio("../Sounds/drum.wav"),
 	bSound = new Audio("../Sounds/housePiano.wav"),
 	//rPiano = new Audio("../Sounds/randomPiano.wav"),
 	//rDrum = new Audio("../Sounds/retroDrum.wav"),
-	ySound = new Audio("../Sounds/saxophone.wav"),
-	source = context.createMediaElementSource(),
+	ySound = new Audio("../Sounds/saxophone.wav");
+
+	//source = context.createMediaElementSource();
+
+	var arrayColor = new Array(4);
+
+		var coord = new Array(16);
+
+		for(var x = 0; x < coord.length; x++){
+			coord[x] = new Array(15);
+		}
 
 if (navigator.requestMIDIAccess) {
 	console.log("This browser supports MIDI");
@@ -14,42 +24,42 @@ if (navigator.requestMIDIAccess) {
 	function onMIDISucces(midiAccess){
 		console.log(midiAccess);
 		
-		for (var input of midiAccess.input.values())
-		input.onmidimessage = getMIDIMessage;
-
 		var inputs = midiAccess.inputs;
 		var outputs =  midiAccess.outputs;
 
+		/*
 		rSound.play();
-		rSound.loop = true;
+		rSound.loop = false;
 		gSound.play();
-		gSound.loop = true;
+		gSound.loop = false;
 		bSound.play();
-		bSound.loop = true;
+		bSound.loop = false;
 		ySound.play();
-		ySound.loop = true;
+		ySound.loop = false;
+		*/
 	}
 
 	function onMIDIFailure(){
-		console.log("Could not acess your MIDI devices.");
+		console.log('Could not acess your MIDI devices.');
 	}
 
 	function getMIDIMessage(midiMessage) {
 		console.log(midiMessage);
+		console.log("hallo");
+
+		rSound.play();
+		rSound.loop = true;
 
 		var startBit = midiMessage.data[0];
-		var colorP = midiMessage.data[1];
+		var color = midiMessage.data[1];
 		var xCoord = midiMessage.data[2];
 		var yCoord = midiMessage.data[3];
 		var endBit = midiMessage.data[4];
 
-		int c;
-		int coord {xCoord} {yCoord} = c;
+		coord [xCoord] [yCoord] = color;
 
-		int color {c};
-		
 		//Timer mit Counter
-		float tCounter = 0;
+		var tCounter = 0;
 
 		/*const timer = setInterval(() => {
 			tCounter += 0.1;
@@ -60,49 +70,30 @@ if (navigator.requestMIDIAccess) {
 		}, 100);
 		*/
 
+		//Variablen für  die for Schleifen
+		var c = 0;
+		var y = 0;
+		var i = 0;
+
 		//Zusammenführung der MidiDaten mit dem Counter 
-		for each(color{} = null){
-			for(int y = 0; y <= yCoord; y++){
-				color[coord {tCounter}{yCoord}] = y;
-				console.log("das ist unser Wert"+ y);
-			}
-			for(int i = 0; i <=color.length; i++){
-				if (color{i} = null){
-					playInstrument(i, color{i});
-				}
-			}
+		for(c = 0; c < color.length; c++){
+			color[c] = 20;
 		}
 	
-	/*
-	for(int i = 0; i = xCoord; i++){
-		switch(color){
-			case 0: //nichts
-				console.log("Keine Farbe");
-				playSound(color);
-				break;
-
-		case 1: //rot
-				console.log("ROT!!");
-				playSound(color);
-				break;
-
-		case 2: //gelb
-				console.log("GELB!!");
-				playSound(color);
-				break;
-
-		case 3: //gruen
-				console.log("GRUEN!!");
-				playSound(color);
-				break;
-
-		case 4: //blau
-				console.log("Blau!!");
-				playSound(color);
-				break;
+		for(y = 0; y < yCoord; y++){
+			color[coord [tCounter][yCoord]] = y;
+			console.log("das ist unser Wert"+ y);
 		}
-	}*/
-		
+
+		for(i = 0; i <color.length; i++){
+			if (color[i] != 20){
+				playInstrument(i, color[i]);
+			}
+		}		
+	}
+
+	function playInstrument(){
+		console.log("jetzt spielt ein instrument");
 	}
 
 }else {
